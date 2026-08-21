1d979993e0e19bb2c58729603942818ee063a192
Ankit Kumar <er.ankitkumar75@gmail.com>
2026-08-21 15:20:18 +0530

    Implement cached API React component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default App
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }, []);
+
+  return (
+    <div id="main">
+      <h1>Posts</h1>
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        posts.map((post) => (
+          <div key={post.id}>
+            <p>{post.title}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default App;
